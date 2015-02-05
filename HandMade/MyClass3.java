b100e4e15e2ecbf930aba20e80aeec91d3148de8
unknown <maxwhite@n103291.merann.ru>
2015-02-05 16:27:00 +0300

    Revert "test3: input processed via BufferedReader"
    This reverts commit 457855d370156deb47ba2cd9325e1d1d1df043d9.

--- HandMade/MyClass3.java
+++ HandMade/MyClass3.java
@@ -1,20 +1,16 @@
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.nio.charset.Charset;
 
 
 public class MyClass3 {
 
     public static void main(String[] args) throws IOException {
+        StringBuilder inputData = new StringBuilder();
         int someByte;
         System.out.println("What is your name?");
-
-        BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in, Charset.forName(System.getProperty("sun.stdout.encoding"))));
-
-        String inputData = buffReader.readLine();
-
-        System.out.printf("Hello %s!\n", inputData);
+        while ((someByte=System.in.read())!='\r') {
+            inputData.append((char)someByte);
+        }
+        System.out.printf("Hello %s!\n", inputData.toString());
     }
 
 }
