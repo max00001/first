38bc5ae01fc877d285deb0329d849af72fa4a2ea
max00001 <max00001@users.noreply.github.com>
2015-02-06 18:00:09 +0300

    Exercise for Collections chapter version 1.0

--- P23ExForCollections/src/mw/p23/main/Main.java
+++ P23ExForCollections/src/mw/p23/main/Main.java
@@ -1,6 +1,7 @@
 package mw.p23.main;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -14,49 +15,61 @@ import java.util.List;
 
 public class Main {
 
-    private static final int MAX_LINES = 1000;
+    private static final int MAX_LINES_TO_OUT = 1000;
+    private static final int AV_LINE_LENGTH = 25; // to calculate lines number
     
     public static void main(String[] args) {
         if (!checkArgs(args)) {
             return;
         }
-        Path filename = Paths.get(args[0]);
-        if (!Files.exists(filename)) {
+        Path filename = Paths.get(args[0]).toAbsolutePath();
+        if (!Files.exists(filename)) {            
             System.out.printf("file %s does't exist.\n", filename);
             printUsage();
             return;
-        } else {
-            System.out.println(filename);
         }
         
-        int linesTotal = Integer.parseInt(args[1]);
-        if (linesTotal > MAX_LINES) {
-            System.out.printf("there is a limit to print no more than %d lines.\n", MAX_LINES);
-            linesTotal = MAX_LINES;
-        } else if (linesTotal < 1) {
+        File file = filename.toFile();
+        double tmpLinesInFile = Math.ceil(file.length()/AV_LINE_LENGTH);
+        System.out.println("Lines in the file (approx.): " + tmpLinesInFile);
+        if (tmpLinesInFile > Integer.MAX_VALUE) {
+            System.out.println("The file is huge. "
+                    + "It cannot be processed"); //TODO - remove the limit
+            return;
+        } 
+        int linesInFile = (int)tmpLinesInFile; //safe thanks to the previous check
+        
+        int linesTotalToOut = Integer.parseInt(args[1]);
+        if (linesTotalToOut > MAX_LINES_TO_OUT) {
+            System.out.printf("there is a limit to print no more than %d lines.\n", MAX_LINES_TO_OUT);
+            linesTotalToOut = MAX_LINES_TO_OUT;
+        } else if (linesTotalToOut < 1) {
             System.out.println("the second argument is less than 1. Let's try to print one line.\n");
-            linesTotal = 1;
+            linesTotalToOut = 1;
         }
         
         try (BufferedReader br = new BufferedReader(new FileReader(filename.toString()
                 ))) {
-            int lineCounter = 0;
-  
-            List<String> strList = new ArrayList<String>(linesTotal);
+              
+            List<String> strList = new ArrayList<String>(linesInFile);
             String aLine;
-            while ((lineCounter++ < linesTotal) && !((aLine=br.readLine()) == null)) {
+            while (!((aLine=br.readLine()) == null)) {
                 strList.add(aLine);
             }
+            if (linesTotalToOut > strList.size()) {
+                linesTotalToOut = strList.size();
+            }
             Collections.shuffle(strList);
-            for (String string : strList) {
+            for (String string : strList.subList(0, linesTotalToOut)) {
                 System.out.println(string);
             }
         } 
-        catch (FileNotFoundException e) {
-            // TODO: handle exception
-            System.out.println(e.getMessage());
-            return;
-        }
+//        FileNotFoundException will be processed as IOException
+//        catch (FileNotFoundException e) {
+//            // TODO: handle exception
+//            System.out.println(e.getMessage());
+//            return;
+//        }
         catch (IOException e) {
             // TODO: handle exception
             System.out.println(e.getMessage());
