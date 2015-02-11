fd360928b8a8625a783dcba0b443ab075ad21c51
max00001 <max00001@users.noreply.github.com>
2015-02-11 15:43:02 +0300

    Commit of an exercise from Collections chapter

--- P23ExForCollections/src/mw/p23/main/Main.java
+++ P23ExForCollections/src/mw/p23/main/Main.java
@@ -2,14 +2,12 @@ package mw.p23.main;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
